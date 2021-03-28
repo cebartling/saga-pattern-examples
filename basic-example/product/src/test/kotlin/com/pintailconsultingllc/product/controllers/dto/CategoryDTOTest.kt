29fc75750cea271832e8bac825646a27f9bc72b5
Christopher Bartling <chris.bartling@gmail.com>
2021-03-28 12:48:25 -0500

    CategoryDTO unit tests

---
+++ basic-example/product/src/test/kotlin/com/pintailconsultingllc/product/controllers/dto/CategoryDTOTest.kt
@@ -0,0 +1,74 @@
+package com.pintailconsultingllc.product.controllers.dto
+
+import com.pintailconsultingllc.product.jpa.entities.Category
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+import java.util.*
+
+@DisplayName("CategoryDTO unit tests")
+internal class CategoryDTOTest {
+
+    private val entity = createCategoryEntity()
+    private var categoryDTO: CategoryDTO? = null
+
+    @Nested
+    @DisplayName("constructor")
+    inner class ContructorTests {
+
+        @BeforeEach
+        fun doBeforeEachTest() {
+            categoryDTO = CategoryDTO(entity = entity)
+        }
+
+        @Test
+        fun `should set the data transfer object's ID`() {
+            assertThat(categoryDTO?.id).isEqualTo(entity.id)
+        }
+
+        @Test
+        fun `should set the data transfer object's name`() {
+            assertThat(categoryDTO?.name).isEqualTo(entity.name)
+        }
+
+        @Test
+        fun `should set the data transfer object's version`() {
+            assertThat(categoryDTO?.version).isEqualTo(entity.version)
+        }
+
+        @Test
+        fun `should set the data transfer object's created at timestamp`() {
+            assertThat(categoryDTO?.createdAt).isEqualTo(entity.createdAt)
+        }
+
+        @Test
+        fun `should set the data transfer object's updated at timestamp`() {
+            assertThat(categoryDTO?.updatedAt).isEqualTo(entity.updatedAt)
+        }
+
+        @Test
+        fun `should set the data transfer object's created by string`() {
+            assertThat(categoryDTO?.createdBy).isEqualTo(entity.createdBy)
+        }
+
+        @Test
+        fun `should set the data transfer object's updated by string`() {
+            assertThat(categoryDTO?.updatedBy).isEqualTo(entity.updatedBy)
+        }
+    }
+
+    private fun createCategoryEntity(): Category {
+        return Category(
+            id = UUID.randomUUID(),
+            name = "foobar category",
+            version = 12,
+            createdAt = LocalDateTime.now(),
+            updatedAt = LocalDateTime.now(),
+            createdBy = "system",
+            updatedBy = "system"
+        )
+    }
+}
--- basic-example/product/src/test/kotlin/com/pintailconsultingllc/product/controllers/v1/ProductControllerIntegrationTest.kt
+++ basic-example/product/src/test/kotlin/com/pintailconsultingllc/product/controllers/v1/ProductControllerIntegrationTest.kt
@@ -1,17 +1,11 @@
 package com.pintailconsultingllc.product.controllers.v1
 
-import com.pintailconsultingllc.product.jpa.entities.Product
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
-
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
-import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductControllerIntegrationTest(
